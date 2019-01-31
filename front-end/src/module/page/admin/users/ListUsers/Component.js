d9ed7c1d78a767cea4a0208ecf64b7a3937b57f6
Harry Liu <harryliu@cyberrepublic.org>
2019-01-31 22:03:23 +0800

    fixed eslint

--- front-end/src/module/page/admin/users/ListUsers/Component.js
+++ front-end/src/module/page/admin/users/ListUsers/Component.js
@@ -1,70 +1,58 @@
 import React from 'react'
+import _ from 'lodash'
 import BaseComponent from '@/model/BaseComponent'
 import { Table, Icon } from 'antd'
 import I18N from '@/I18N'
 import config from '@/config'
 
 import './style.scss'
-import moment from 'moment/moment'
 
 export default class extends BaseComponent {
+  ord_render() {
+    const columns = [{
+      title: I18N.get('1201'),
+      dataIndex: 'username',
+      render: (username, record) => <a onClick={this.linkProfileInfo.bind(this, record._id)} className="tableLink">{username}</a>,
+      sorter: (a, b) => a.username.localeCompare(b.username),
+      defaultSortOrder: 'ascend',
+    }, {
+      title: I18N.get('1208'),
+      dataIndex: '_id',
+      render: (id, item) => `${item.profile.firstName} ${item.profile.lastName}`,
 
-    ord_render () {
-        const columns = [{
-            title: I18N.get('1201'),
-            dataIndex: 'username',
-            render: (username, record) => {
-                return <a onClick={this.linkProfileInfo.bind(this, record._id)} className="tableLink">{username}</a>
-            },
-            sorter: (a, b) => {
-                return a.username.localeCompare(b.username)
-            },
-            defaultSortOrder: 'ascend'
-        }, {
-            title: I18N.get('1208'),
-            dataIndex: '_id',
-            render: (id, item) => {
-                return item.profile.firstName + ' ' + item.profile.lastName
-            }
+    }, {
+      title: I18N.get('1202'),
+      dataIndex: 'email',
+    }, {
+      title: I18N.get('1209'),
+      dataIndex: 'profile.country',
+      render: (geolocation, item) => config.data.mappingCountryCodeToName[geolocation],
+      sorter: (a, b) => config.data.mappingCountryCodeToName[a.profile.country].localeCompare(config.data.mappingCountryCodeToName[b.profile.country]),
+    }, {
+      title: I18N.get('1204'),
+      dataIndex: 'role',
+      render: (role) => {
+        if (role === 'LEADER') {
+          role = 'ORGANIZER'
+        }
 
-        }, {
-            title: I18N.get('1202'),
-            dataIndex: 'email',
-        }, {
-            title: I18N.get('1209'),
-            dataIndex: 'profile.country',
-            render: (geolocation, item) => {
-                return config.data.mappingCountryCodeToName[geolocation]
-            },
-            sorter: (a, b) => {
-                return config.data.mappingCountryCodeToName[a.profile.country].localeCompare(config.data.mappingCountryCodeToName[b.profile.country])
-            }
-        }, {
-            title: I18N.get('1204'),
-            dataIndex: 'role',
-            render: (role) => {
-                if (role === 'LEADER') {
-                    role = 'ORGANIZER'
-                }
+        return _.capitalize(role)
+      },
+    }]
 
-                return _.capitalize(role)
-            }
-        }]
+    const data = this.props.users
 
-        const data = this.props.users
+    return (
+      <Table
+        columns={columns}
+        dataSource={data}
+        rowKey={record => record.username}
+        loading={this.props.loading}
+      />
+    )
+  }
 
-        return (
-            <Table
-                columns={columns}
-                dataSource={data}
-                rowKey={record => record.username}
-                loading={this.props.loading}
-            />
-        )
-    }
-
-    linkProfileInfo(userId) {
-        this.props.history.push(`/admin/profile/${userId}`)
-    }
-
+  linkProfileInfo(userId) {
+    this.props.history.push(`/admin/profile/${userId}`)
+  }
 }
